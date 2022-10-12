e278dcd8241aa6fb656d3b064c56c12aede35d1b
P1roks <piotrekjakobczyk1@gmail.com>
2022-10-12 22:32:31 +0200

    Working login and register

--- src/structs.rs
+++ src/structs.rs
@@ -1,5 +1,6 @@
+use bcrypt::{hash_with_salt, DEFAULT_COST};
+use rand::Rng;
 pub use rocket::serde::json::Json;
-use rocket_db_pools::sqlx::{query, Row};
 pub use rocket_db_pools::{sqlx, Database};
 
 use anyhow;
@@ -42,16 +43,20 @@ impl CustomError {
 }
 pub type JsonCustomError = Json<CustomError>;
 pub type CustomResult<T> = Result<T, Json<CustomError>>;
+
 impl UserLogin<'_> {
     pub async fn login(&self, db: &sqlx::MySqlPool) -> anyhow::Result<bool> {
-        let salt = sqlx::query!("SELECT salt as salt FROM users WHERE name = ?")
+        let salt: (String,) = sqlx::query_as("SELECT salt as salt FROM users WHERE name = ?")
             .bind(self.name)
-            .fetch_optional(db)
-            .await?
-            .salt;
+            .fetch_one(db)
+            .await?;
+
+        let salt: [u8; 16] = salt.0.into_bytes().try_into().unwrap();
+        let hashed_pass = hash_with_salt(self.password.as_bytes(), DEFAULT_COST, salt)?.to_string();
+
         let logged = sqlx::query("SELECT id FROM users WHERE name = ? AND password = ?")
             .bind(self.name)
-            .bind(self.password)
+            .bind(hashed_pass)
             .fetch_optional(db)
             .await?;
 
@@ -64,12 +69,22 @@ impl UserLogin<'_> {
 
 impl User<'_> {
     pub async fn add_to_db(&self, db: &sqlx::MySqlPool) -> anyhow::Result<bool> {
+        let salt = {
+            let rand_string: Vec<u8> = rand::thread_rng()
+                .sample_iter(&rand::distributions::Alphanumeric)
+                .take(16)
+                .collect();
+            rand_string
+        };
+        let copy_salt: [u8; 16] = salt.clone().try_into().unwrap();
+        let hashed_pass = hash_with_salt(self.login.password.as_bytes(), DEFAULT_COST, copy_salt)?;
+
         let rows_affected =
-            sqlx::query("INSERT INTO users (email, name, password,salt) VALUES (?, ?, ?,?)")
+            sqlx::query("INSERT INTO users (email, name, password, salt) VALUES (?, ?, ?, ?)")
                 .bind(self.email)
                 .bind(self.login.name)
-                .bind(self.login.password)
-                .bind("sul")
+                .bind(hashed_pass.to_string())
+                .bind(salt)
                 .execute(db)
                 .await?
                 .rows_affected();
