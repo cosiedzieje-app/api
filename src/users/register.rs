7a650ad042177e38691866b150b260b37ad35296
Wredi <piotr.skora69@gmail.com>
2022-10-14 19:33:56 +0200

    fixed clippy error

--- src/users/register.rs
+++ src/users/register.rs
@@ -61,7 +61,7 @@ fn validate_postal_code(postal_code: &str) -> Result<(), ValidationError> {
 impl UserRegister<'_> {
     pub async fn add_to_db(&self, db: &sqlx::MySqlPool) -> anyhow::Result<bool> {
         let salt = nanoid!(16);
-        let salt_copy: [u8; 16] = salt.as_bytes().clone().try_into().unwrap();
+        let salt_copy: [u8; 16] = salt.clone().as_bytes().try_into().unwrap();
         let hashed_pass = hash_with_salt(self.login.password.as_bytes(), DEFAULT_COST, salt_copy)?;
 
         let mut tx = db.begin().await?;
